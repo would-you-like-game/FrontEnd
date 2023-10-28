fd4d9f0c5a25d21586a5b70b9f473e18de4e740c
HyeonE <mag30102000@gmail.com>
2023-10-28 19:22:50 +0900

    feat: add signup functionality

--- src/pages/signup/view/signupContent/index.tsx
+++ src/pages/signup/view/signupContent/index.tsx
@@ -1,23 +1,151 @@
-import { Input } from '@/common/input';
+import { useState } from 'react';
 import * as s from './style';
+import { Button, Input } from '@/common';
+import { useForm } from 'react-hook-form';
+import {
+  handleEmailCheck,
+  handleEmailKeyCheck,
+  handleNicknameCheck,
+  onSubmitSignup,
+} from '@/apis/signupApi';
 
-const SignupContent = () => {
+export const SignupContent = () => {
+  const [emailCheck, setEmailCheck] = useState<boolean>(false);
+  const [emailAuth, setEmailAuth] = useState<boolean>(false);
+  const [nicknameAuth, setNicknameAuth] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const handleSignupSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (emailAuth && nicknameAuth) {
+      console.log('폼버튼 성공');
+      handleSubmit(onSubmitSignup)();
+    }
+  };
+
   return (
-    <s.SignupContent>
-      <Input type="email" placeholder="Enter your e-mail">
-        E-mail
-      </Input>
-      <Input type="text" placeholder="Enter your nickname">
-        Nickname
-      </Input>
-      <Input type="password" placeholder="Enter your password">
+    <s.SignupContent
+      onSubmit={(e) => {
+        handleSignupSubmit(e);
+      }}
+    >
+      <s.Validate>
+        <Input
+          type="email"
+          register={register('email', { required: true })}
+          placeholder="이메일을 입력해주세요"
+          readOnly={emailCheck}
+        >
+          E-mail
+        </Input>
+        {!emailCheck && (
+          <Button
+            onClick={() => {
+              handleEmailCheck(watch('email'), setEmailCheck);
+            }}
+          >
+            이메일 인증
+          </Button>
+        )}
+      </s.Validate>
+      {emailCheck && (
+        <s.Validate>
+          <Input
+            type="text"
+            register={register('emailValidate', { required: true })}
+            placeholder="인증코드를 입력해주세요"
+            readOnly={emailAuth}
+          >
+            E-mail Verification
+          </Input>
+          {!emailAuth && (
+            <Button
+              onClick={() => {
+                handleEmailKeyCheck(
+                  watch('email'),
+                  watch('emailValidate'),
+                  setEmailAuth
+                );
+              }}
+            >
+              인증번호 확인
+            </Button>
+          )}
+        </s.Validate>
+      )}
+      <s.Validate>
+        <Input
+          type="text"
+          register={register('nickname', {
+            pattern: {
+              value: /^.{1,8}$/,
+              message: '닉네임은 1자 이상 8자 이하여야 합니다',
+            },
+          })}
+          placeholder="닉네임을 입력해주세요"
+          readOnly={nicknameAuth}
+        >
+          Nickname
+        </Input>
+        {!nicknameAuth && (
+          <Button
+            onClick={() => {
+              handleNicknameCheck(watch('nickname'), setNicknameAuth);
+            }}
+          >
+            닉네임 확인
+          </Button>
+        )}
+      </s.Validate>
+      {errors.nickname && typeof errors.nickname.message === 'string' && (
+        <s.ValidateTexT>{errors.nickname.message}</s.ValidateTexT>
+      )}
+      <Input
+        type="password"
+        register={register('password', {
+          pattern: {
+            value: /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^<>&*]).{8,20}$/i,
+            message:
+              '비밀번호는 8자 이상 20자 미만 대소문자, 숫자, 특수문자(!@#$%"<>"^&*)를 각 1개 이상씩 사용해야합니다',
+          },
+        })}
+        placeholder="비밀번호를 입력해주세요"
+      >
         Password
       </Input>
-      <Input type="password" placeholder="Re-enter your password">
+      {errors.password && typeof errors.password.message === 'string' && (
+        <s.ValidateTexT>{errors.password.message}</s.ValidateTexT>
+      )}
+      <Input
+        type="password"
+        register={register('passwordVerification', {
+          validate: {
+            matchPassword: (value: string) => {
+              return (
+                watch('password') === value || '비밀번호가 일치하지 않습니다'
+              );
+            },
+          },
+        })}
+        placeholder="비밀번호를 다시 입력해주세요"
+      >
         Password Verification
       </Input>
+      {errors.passwordVerification &&
+        typeof errors.passwordVerification.message === 'string' && (
+          <s.ValidateTexT>{errors.passwordVerification.message}</s.ValidateTexT>
+        )}
+      <s.ButtonContainer>
+        <Button color="black2" size="medium" type="submit">
+          Signup
+        </Button>
+      </s.ButtonContainer>
     </s.SignupContent>
   );
 };
-
-export default SignupContent;
