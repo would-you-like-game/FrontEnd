c65754ba655a53b1f9452ded14a0c9906806f33e
HyeonE <56420106+hyeon12@users.noreply.github.com>
2023-10-31 00:25:38 +0900

    test: login token undefined

--- src/apis/loginApi.ts
+++ src/apis/loginApi.ts
@@ -5,7 +5,9 @@ export const onSubmitLogin = async (data) => {
     const response = await api.post('auth/login', data);
     if (response.status === 200) {
       const token = response.data.token;
+      console.log('token', token, response.data.token)
       localStorage.setItem('token', token);
+      console.log('token', token, response.data.toke)
       window.location.replace('/');
     }
   } catch (error) {
