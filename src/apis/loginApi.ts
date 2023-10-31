cd4eec22cdabf55b0b7afb0a53fd8065ef26a634
kang sin beom <83047601+kangsinbeom@users.noreply.github.com>
2023-10-31 13:06:37 +0900

    Update loginApi.ts

--- src/apis/loginApi.ts
+++ src/apis/loginApi.ts
@@ -2,9 +2,12 @@ import { api } from './api';
 
 export const onSubmitLogin = async (data) => {
   try {
+    const URL = import.meta.env.VITE_URL;
+    console.log(URL)
     const response = await api.post('auth/login', data);
     if (response.status === 200) {
       const token = response.data.token;
+      
       console.log('response', response);
       localStorage.setItem('token', token);
       console.log('token', token, response.data.toke)
