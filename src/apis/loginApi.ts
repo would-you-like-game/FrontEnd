0a2e951da19beaef6823e26766bf113ce4cc06f3
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-10-31 13:33:14 +0900

    feat: add .env

--- src/apis/loginApi.ts
+++ src/apis/loginApi.ts
@@ -1,12 +1,17 @@
 import { api } from './api';
 
 export const onSubmitLogin = async (data) => {
+  const URL = import.meta.env.VITE_URL;
+
+  console.log(URL, 'zzz');
   try {
     const response = await api.post('auth/login', data);
     if (response.status === 200) {
       const token = response.data.token;
+      console.log('response', response);
       localStorage.setItem('token', token);
-      window.location.replace('/');
+      console.log('token', token, response.data.token);
+      // window.location.replace('/');
     }
   } catch (error) {
     console.error('error', error);
