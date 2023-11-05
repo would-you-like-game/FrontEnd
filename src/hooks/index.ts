83497fdb37571942e92f428ce3054e9cc0f0a3a2
HyeonE <mag30102000@gmail.com>
2023-11-06 00:00:13 +0900

    rename: rename usenavigate to usenavigateto

---
+++ src/hooks/index.ts
@@ -0,0 +1,7 @@
+export * from './useGetPathname';
+export * from './useInput';
+export * from './useModal';
+export * from './usePagination';
+export * from './useRecoilInput';
+export * from './useToggle';
+export * from './useNavigateTo';
