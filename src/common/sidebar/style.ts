8c244b6f2b5a52d958595e780a677b8342e793e4
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-11-06 20:41:07 +0900

    fix: sidebar and header featuer

--- src/common/sidebar/style.ts
+++ src/common/sidebar/style.ts
@@ -4,6 +4,7 @@ import styled from '@emotion/styled';
 export const Sidebar = styled.div`
   display: flex;
   min-width: 220px;
+  height: 100%;
   flex-direction: column;
   background-color: ${Theme.colors.gray1};
   justify-content: space-between;
