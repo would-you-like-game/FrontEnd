8fdfeb8a1c1f06b9f21a5bdb1d9bc8294f83b3f7
HyeonE <mag30102000@gmail.com>
2023-11-09 20:30:38 +0900

    feat: add mypage functionality

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,16 +1,17 @@
 import * as s from './style';
 import { Sidebar } from '@/common';
 import { ContentArea } from '@/styles/GlobalStyle';
-import { PostList } from './components';
+import { useGetPathname } from '@/hooks';
+import { Profile } from './view';
+import { Outlet } from 'react-router-dom';
 
 export const Mypage = () => {
+  const pathname = useGetPathname();
   return (
     <s.Mypage>
       <Sidebar />
       <ContentArea>
-        <s.Right>
-          <PostList />
-        </s.Right>
+        {pathname[1] === undefined ? <Profile /> : <Outlet />}
       </ContentArea>
     </s.Mypage>
   );
