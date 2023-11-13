f6e281511a08b233c25a1a56221c5584dc7857a2
HyeonE <mag30102000@gmail.com>
2023-11-13 22:36:59 +0900

    feat: add common useInputHook functionality

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,19 +1,29 @@
-import { ChangeEvent, useCallback, useState } from 'react';
+import {
+  Dispatch,
+  SetStateAction,
+  ChangeEvent,
+  useCallback,
+  useState,
+} from 'react';
 
 export const useInput = <T>(
   initialState: T
-): [T, (event: ChangeEvent<HTMLInputElement>) => void] => {
+): [
+  T,
+  (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
+  Dispatch<SetStateAction<T>>,
+] => {
   const [form, setForm] = useState<T>(initialState);
 
   const onChange = useCallback(
-    (event: ChangeEvent<HTMLInputElement>) => {
+    (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
       const value = event.target.value as unknown as T;
 
       setForm(value);
     },
     [setForm]
   );
-  return [form, onChange];
+  return [form, onChange, setForm];
 };
 /** 사실상 target안에 있는 value가 string이기때문에 string으로 고정해도 되지만
  * 일단은 저렇게 제너럴함수 써놓고 나중에 필요없으면 고치자 */
