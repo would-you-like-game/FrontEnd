83f834f9ee139f2cfd1274195e83563ac8728c56
HyeonE <mag30102000@gmail.com>
2023-11-13 22:35:23 +0900

    design: change chatting ui

--- src/pages/chatting/view/chattingMiddle/style.ts
+++ src/pages/chatting/view/chattingMiddle/style.ts
@@ -3,7 +3,8 @@ import styled from '@emotion/styled';
 export const ChattingMiddle = styled.div`
   display: flex;
   flex-direction: column;
+  max-height: calc(100% - 100px);
   gap: 25px;
-  height: 100%;
   padding: 50px 50px 30px 50px;
+  overflow: auto;
 `;
