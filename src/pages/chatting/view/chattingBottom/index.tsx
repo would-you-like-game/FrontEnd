00b614ddb3a833c56b5c0eba55b498fae103a34a
HyeonE <mag30102000@gmail.com>
2023-11-13 23:58:00 +0900

    fix: message not being sent on button click

--- src/pages/chatting/view/chattingBottom/index.tsx
+++ src/pages/chatting/view/chattingBottom/index.tsx
@@ -2,20 +2,17 @@ import { Button } from '@/common';
 import * as s from './style';
 import { ChattingInput } from '../../components';
 
-export const ChattingBottom = ({
-  msg,
-  onChangeMsg,
-  onKeyDown,
-  onSendMsg,
-}) => {
+export const ChattingBottom = ({ msg, onChangeMsg, onKeyDown, onSendMsg }) => {
   return (
     <s.chattingBottom>
-      <ChattingInput
-        value={msg}
-        onChange={onChangeMsg}
-        onKeyDown={onKeyDown}
-      />
-      <Button borderColor="black2" color="black2" onClick={onSendMsg}>
+      <ChattingInput value={msg} onChange={onChangeMsg} onKeyDown={onKeyDown} />
+      <Button
+        borderColor="black2"
+        color="black2"
+        onClick={() => {
+          onSendMsg(msg);
+        }}
+      >
         Send
       </Button>
     </s.chattingBottom>
