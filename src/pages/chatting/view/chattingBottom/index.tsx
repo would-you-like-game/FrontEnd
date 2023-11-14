0689f2410da2b457fcdcce787d783b1269ff6b2c
HyeonE <mag30102000@gmail.com>
2023-11-15 00:13:46 +0900

    fix: issue with sending message on enter key and resolving extra '\n'

--- src/pages/chatting/view/chattingBottom/index.tsx
+++ src/pages/chatting/view/chattingBottom/index.tsx
@@ -2,10 +2,21 @@ import { Button } from '@/common';
 import * as s from './style';
 import { ChattingInput } from '../../components';
 
-export const ChattingBottom = ({ msg, onChangeMsg, onKeyDown, onSendMsg }) => {
+export const ChattingBottom = ({
+  msg,
+  setMsg,
+  onChangeMsg,
+  onKeyDown,
+  onSendMsg,
+}) => {
   return (
     <s.chattingBottom>
-      <ChattingInput value={msg} onChange={onChangeMsg} onKeyDown={onKeyDown} />
+      <ChattingInput
+        value={msg}
+        setMsg={setMsg}
+        onChange={onChangeMsg}
+        onKeyDown={onKeyDown}
+      />
       <Button
         borderColor="black2"
         color="black2"
